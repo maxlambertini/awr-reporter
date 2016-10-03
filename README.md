844cf1fa62a6b4a89c1d3319f9e2572e7a597b6d
Max Lambertini <m.lambertini@gmail.com>
2016-10-03 16:57:47 +0200

    Handles awr compare

--- README.md
+++ README.md
@@ -24,52 +24,47 @@ And then you're set.
 ```
 awr.pl -- A perl wrapper for AWR Report. (C) 2016 Max Lambertini (m.lambertini\@gmail.com)
 
-USAGE: ./awr.pl [options]
-    --dbuser=[user]  : Oracle user
-    --dbpass=[pass]  : Oracle password
-    --dbinst=[num]   : DB Instance (default = 1)
-    --dbhost=[host]  : TNS Alias, if needed
 
-    --begin=[num]    : starting awr snapshot id
-    --begind=[date]  : begin interval date
-    --end=[num]      : ending awr snapshot id
-    --endd=[date]    : end interval date
-    
+USAGE: ./awr.pl [options]
+    -u, --dbuser=[user]  : Oracle user
+    -p, --dbpass=[pass]  : Oracle password
+    -n, --dbinst=[num]   : DB Instance (default = 1)
+    -h, --dbhost=[host]  : TNS Alias, if needed
+    -s, --begin=[num]    : starting awr snapshot id
+    -b, --begind=[date]  : begin interval date
+    -f, --end=[num]      : ending awr snapshot id
+    -e, --endd=[date]    : end interval date
+    -c, --begind2=[date] : begin 2nd interval date (for awr comparison)
+    -k, --endd2=[date]   : end 2nd interval date (for awr comparison)
+
     NOTE: Dates must comply to this mask: YYYYMMDD_HH24:MI:SS. For instance, 20160920_19:34:41
     
-    --output[fname]  : output filename. A standard filename will be generated if empty
+    -o, --output[fname]  : output filename. A standard filename will be generated if empty
     
     Flags:
     
-    --dba            : connect as sysdba
-    --global         : when generating reports on a RAC, generates a global report
-    --text           : generate a text report instead of an HTML one
-
-    --help           : show this screen
+    -a, --dba            : connect as sysdba
+    -G, --global         : when generating reports on a RAC, generates a global report
+    -t, --text           : generate a text report instead of an HTML one
+    -h, --help           : show this screen
     
 Sample usage:
--------------
 
-Generate an awr report between two dates as sysba
-
-awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 
+Generate an awr report between two dates as sysba 
+ awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 
 
 Same report in a text format
-
-awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --text
+ awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --text
     
 Same report on rac instance 2
-
-awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --dbinst=2
+ awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --dbinst=2
     
 Same date interval, global RAC report
-
-awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --global
+ awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --global
     
 Same report using system user, specifiying a tnsnames alias and a custom output file
+ awr.pl --begind=20160927_11:50:00 --endd=20160927_14:15:00 --dbuser=system --dbpass=Password01 --dbhost=ORA12 --output=custom.html
 
-awr.pl --begind=20160927_11:50:00 --endd=20160927_14:15:00 --dbuser=system --dbpass=Password01 --dbhost=ORA12 --output=custom.html
-    
 
 ```
 
--- awr.pl
+++ awr.pl
@@ -18,101 +18,123 @@ my $sid =  $ENV{'ORACLE_SID'};
 
 my $begin       = -1;
 my $end         = -1;
-my $begin2      = undef;
-my $end2        = undef;
-my $begin_date  = undef;
-my $end_date    = undef;
-my $begin_date2 = undef;
-my $end_date2   = undef;
+my $begin2      = -1;
+my $end2        = -1;
+my $begin_date  = '';
+my $end_date    = '';
+my $begin_date2 = '';
+my $end_date2   = '';
 my $o_file      = "output.html";
 my $usage;
 my $global;
 my $text;
+my $verbose     = '';
 
 my $usage_txt = qq{
 awr.pl -- A perl wrapper for AWR Report. (C) 2016 Max Lambertini (m.lambertini\@gmail.com)
 
 USAGE: ./awr.pl [options]
-    --dbuser=[user]  : Oracle user
-    --dbpass=[pass]  : Oracle password
-    --dbinst=[num]   : DB Instance (default = 1)
-    --dbhost=[host]  : TNS Alias, if needed
-
-    --begin=[num]    : starting awr snapshot id
-    --begind=[date]  : begin interval date
-    --end=[num]      : ending awr snapshot id
-    --endd=[date]    : end interval date
-
-    --begind2=[date] : begin 2nd interval date (for awr comparison)
-    --endd2=[date]   : end 2nd interval date (for awr comparison)
+    -u, --dbuser=[user]  : Oracle user
+    -p, --dbpass=[pass]  : Oracle password
+    -n, --dbinst=[num]   : DB Instance (default = 1)
+    -h, --dbhost=[host]  : TNS Alias, if needed
+    -s, --begin=[num]    : starting awr snapshot id
+    -b, --begind=[date]  : begin interval date
+    -f, --end=[num]      : ending awr snapshot id
+    -e, --endd=[date]    : end interval date
+    -c, --begind2=[date] : begin 2nd interval date (for awr comparison)
+    -k, --endd2=[date]   : end 2nd interval date (for awr comparison)
 
     NOTE: Dates must comply to this mask: YYYYMMDD_HH24:MI:SS. For instance, 20160920_19:34:41
     
-    --output[fname]  : output filename. A standard filename will be generated if empty
+    -o, --output[fname]  : output filename. A standard filename will be generated if empty
     
     Flags:
     
-    --dba            : connect as sysdba
-    --global         : when generating reports on a RAC, generates a global report
-    --text           : generate a text report instead of an HTML one
-
-    --help           : show this screen
+    -a, --dba            : connect as sysdba
+    -G, --global         : when generating reports on a RAC, generates a global report
+    -t, --text           : generate a text report instead of an HTML one
+    -h, --help           : show this screen
     
 Sample usage:
--------------
 
-Generate an awr report between two dates as sysba
-
-awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 
+Generate an awr report between two dates as sysba 
+ awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 
 
 Same report in a text format
-
-awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --text
+ awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --text
     
 Same report on rac instance 2
-
-awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --dbinst=2
+ awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --dbinst=2
     
 Same date interval, global RAC report
-
-awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --global
+ awr.pl --dba --begind=20160927_11:50:00 --endd=20160927_14:15:00 --global
     
 Same report using system user, specifiying a tnsnames alias and a custom output file
-
-awr.pl --begind=20160927_11:50:00 --endd=20160927_14:15:00 --dbuser=system --dbpass=Password01 --dbhost=ORA12 --output=custom.html
+ awr.pl --begind=20160927_11:50:00 --endd=20160927_14:15:00 --dbuser=system --dbpass=Password01 --dbhost=ORA12 --output=custom.html
     
 };
 
 
 
-GetOptions("begin=i"  => \$begin,
-           "begin2=i" => \$begin2,
-           "begind=s" => \$begin_date,
-           "begind2=s"=> \$begin_date2,
-           "endd=s"   => \$end_date,
-           "endd2=s"  => \$end_date2
-           "end=i"    => \$end,
-           "end2=i"   => \$end2,
-           "dbuser=s" => \$dbuser,
-           "dbpass=s" => \$dbpass,
-           "dbinst=i" => \$inst_id,
-           "dbhost=s" => \$dbhost,
-           "output=s" => \$o_file,
-           "dba"      => \$dba,
-           "global"   => \$global,
-           "text"     => \$text,
-           "help"     => \$usage)
+  
+GetOptions("begin|s=i"  => \$begin,
+           "begin2|z=i" => \$begin2,
+           "begind|b=s" => \$begin_date,
+           "begind2|c=s"=> \$begin_date2,
+           "endd|e=s"   => \$end_date,
+           "endd2|k=s"  => \$end_date2,
+           "end|f=i"    => \$end,
+           "end2|j=i"   => \$end2,
+           "dbuser|u=s" => \$dbuser,
+           "dbpass|p=s" => \$dbpass,
+           "dbinst|n=i" => \$inst_id,
+	       "dbhost|h=s" => \$dbhost,
+           "output|o=s" => \$o_file,
+           "dba|a"      => \$dba,
+           "global|g"   => \$global,
+           "text|t"     => \$text,
+           "verbose|v"  => \$verbose,
+           "help|h"     => \$usage)
 or die ("Error in command line argument. awr.pl --help for usage info \n");
 
+if ($verbose) {
+	my $dump = qq(
+Parameters passed:
+--------------------------------------------------
+Begin Snapshot..........: $begin
+Begin Snapshot2.........: $begin2
+Begin Date..............: $begin_date
+Begin Date 2............: $begin_date2
+End Date................: $end_date
+End Date 2..............: $end_date2
+End Snapshot............: $end
+End Snapshot2...........: $end2
+DB User.................: $dbuser
+DB Pass.................: $dbpass
+DB Inst.................: $inst_id
+DB Host.................: $dbhost
+Outout..................: $o_file
+Sysdba..................: $dba
+Global report...........: $global
+Text mode...............: $text
+Verbose mode............: $verbose
+Help....................: $usage
+--------------------------------------------------
+
+);
+	print "$dump\n";
+}
+
 # if true, then generate awr report compare
-if ($text) { $text="txt"; } else { $text="html"; }
+if ($text) { $text="text"; } else { $text="html"; }
 
 if ($usage) {
         print $usage_txt;
         die("\n");
 }
 
-$dbhost = $dbhost ? "dbi:Oracle:" : "dbi:Oracle:" . $dbhost;
+$dbhost = $dbhost eq '' ? "dbi:Oracle:" : "dbi:Oracle:" . $dbhost;
 
 print "Host is $dbhost\n";
 
@@ -160,11 +182,14 @@ if ($begin_date && $end_date) {
                 select a.begin_snap, b.end_snap
                 from (select max(snap_id) begin_snap
           from dba_hist_snapshot
-                 where begin_interval_time <= to_date('$begin_date','YYYY-MM-DD_HH24:MI:SS')) a,
+                 where begin_interval_time <= to_date('$begin_date','YYYYMMDD_HH24:MI:SS')) a,
                 (select min(snap_id) end_snap
           from dba_hist_snapshot
-                 where end_interval_time >= to_date('$end_date','YYYY-MM-DD_HH24:MI:SS')) b
+                 where end_interval_time >= to_date('$end_date','YYYYMMDD_HH24:MI:SS')) b
         };
+		if ($verbose) { 
+			print "snap id from date:\n$the_sql\n\n";
+		}
         $sth1 = $dbh->prepare ($the_sql);
         $sth1->execute();
         @data = $sth1->fetchrow_array();
@@ -179,11 +204,15 @@ if ($begin_date2 && $end_date2) {
                 select a.begin_snap, b.end_snap
                 from (select max(snap_id) begin_snap
           from dba_hist_snapshot
-                 where begin_interval_time <= to_date('$begin_date2','YYYY-MM-DD_HH24:MI:SS')) a,
+                 where begin_interval_time <= to_date('$begin_date2','YYYYMMDD_HH24:MI:SS')) a,
                 (select min(snap_id) end_snap
           from dba_hist_snapshot
-                 where end_interval_time >= to_date('$end_date2','YYYY-MM-DD_HH24:MI:SS')) b
+                 where end_interval_time >= to_date('$end_date2','YYYYMMDD_HH24:MI:SS')) b
         };
+
+		if ($verbose) { 
+			print "snap id from date:\n$the_sql\n\n";
+		}
         $sth1 = $dbh->prepare ($the_sql);
         $sth1->execute();
         @data = $sth1->fetchrow_array();
@@ -202,7 +231,7 @@ if ($begin == -1 || $end == -1)  {
         $sth1->finish();
 }
 
-my $compare_reports = ($begin_date2 && $end_date2);
+my $compare_reports = ($begin2 != -1 && $end2 != -1);
 
 
 if (!$compare_reports) {
@@ -233,7 +262,7 @@ if (!$compare_reports) {
     }   
 } else {
     if ($global) {
-         $sql = "select output from table(dbms_workload_repository.awr_global_report_$text ('$dbid','',$begin,$end,$dbid,'', $begin2, $end2))";
+         $sql = "select output from table(dbms_workload_repository.awr_global_diff_report_$text ('$dbid','',$begin,$end,$dbid,'', $begin2, $end2))";
         print "$sql\n";
     } else {
         $sql = "select output from table(dbms_workload_repository.awr_diff_report_$text ($dbid,$inst_id,$begin,$end,$dbid,$inst_id, $begin2, $end2))";
